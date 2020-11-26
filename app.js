c3f259fbeb9ba41ebb2e8d06156267dabc6b764a
SH-lima <halimasaid.pro@gmail.com>
2020-11-26 23:44:34 +0100

    final commit

--- app.js
+++ app.js
@@ -63,3 +63,45 @@ const sr=ScrollReveal( {
  ScrollReveal().reveal( '#formations p ',{
     delay : 10
 });
+
+
+
+
+
+// dark mode 
+const button = document.querySelector("button")
+const section = document.querySelectorAll("section")
+const titleh3 = document.querySelector("h3")
+const titleh4 = document.querySelector("h4")
+const cards = document.querySelectorAll(".card")
+const mainDark = document.querySelector("main")
+const headerDark = document.querySelector("header")
+const navDark = document.querySelector("#navBar")
+const footerDark = document.querySelector("footer")
+const navElements = document.querySelectorAll("#navBar li")
+
+button.addEventListener("click", ()=>{
+    
+    titleh3.classList.toggle("h3Dark")
+    titleh4.classList.toggle("h4Dark")
+    
+    for (let index = 0; index < section.length; index++) {
+        const element = section[index];
+        element.classList.toggle("bodyDark")
+    }
+    for (let a = 0; a < cards.length; a++) {
+        const card = cards[a];
+        card.classList.toggle("cardDark")
+        
+    }
+    for (let b = 0; b < navElements.length; b++) {
+        const element = navElements[b];
+        element.classList.toggle('bleuDark')
+        
+    }
+    debugger;
+    mainDark.classList.toggle("mainDark")
+    headerDark.classList.toggle("headerDark")
+    navDark.classList.toggle('bleuDark')
+    footerDark.classList.toggle('bleuDark')
+})
